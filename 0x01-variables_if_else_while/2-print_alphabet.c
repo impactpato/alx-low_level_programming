650e741f1d53f8d9d620ca9f1fd95f81c5aa6f9b
impactpato <impactpato@gmail.com>
2022-07-04 15:02:31 -0700

    i tire

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,43 @@
+
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: 0 (success)
+ *
+ *     *
+ *
+ *      */
+
+
+
+int main(void)
+
+{
+
+		int ch;
+
+
+
+			for (ch = 'a'; ch <= 'z'; ch++)
+
+					{
+
+								putchar(ch);
+
+										getchar();
+
+											}
+
+				putchar('\n');
+
+					return (0);
+
+}
