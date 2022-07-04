05f2145d5b4589ae8c7712b72fb91697d3b49724
impactpato <impactpato@gmail.com>
2022-07-04 15:19:07 -0700

    i dont understand

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,35 @@
+nclude <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		int i;
+
+
+
+			for (i = 0; i <= 9; i++)
+
+					{
+
+								printf("%d", i);
+
+									}
+
+				putchar('\n');
+
+					return (0);
+
+}
