0d190fcbb354f2309ea61abff5fc197fd5afa0b8
impactpato <impactpato@gmail.com>
2022-07-04 15:13:18 -0700

    hey

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (success)
+ *
+ *     */
+
+
+
+int main(void)
+
+{
+
+		char i;
+
+
+
+			for (i = 'a'; i <= 'z'; i++)
+
+					{
+
+								if (i == 'q')
+
+											{
+
+															continue;
+
+																	}
+
+										else if (i == 'e')
+
+													{
+
+																	continue;
+
+																			}
+
+												else
+
+															{
+
+																			putchar(i);
+
+																					}
+
+													}
+
+				putchar('\n');
+
+
+
+					return (0);
+
+}
