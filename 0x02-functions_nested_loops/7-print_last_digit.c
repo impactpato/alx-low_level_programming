937a54588c5407e050925e56c6c9ef17849a1eef
impactpato <impactpato@gmail.com>
2022-07-05 20:16:52 -0700

    push

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+
+/**
+ *print_last_digit - gets the last figure form an array
+ *@n: is the int that will use for the function
+ *
+ *Return: 0 or 1
+ */
+
+
+
+int print_last_digit(int n)
+
+{
+
+		int last;
+		last = n % 10;
+		if (last < 0)
+		{
+			last = last * -1;
+
+		}
+		_putchar(last + '0');
+		return (last);
+}
