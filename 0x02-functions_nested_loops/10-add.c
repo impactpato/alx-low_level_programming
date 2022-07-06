6d1564d4d0d369b81183ae50eca9f68c5c7707f4
impactpato <impactpato@gmail.com>
2022-07-05 20:44:51 -0700

    lush

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+
+/**
+ *add - returns the sum
+ *
+ *Return: 0 (success)
+ *@a: are number input parameter to the function
+ *@b: is thr second input
+ */
+
+
+
+int add(int a, int b)
+
+{
+
+		int result = a + b;
+		
+		return (result);
+
+}
