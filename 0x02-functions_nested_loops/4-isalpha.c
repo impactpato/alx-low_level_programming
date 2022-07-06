9be75180d61f79685893081a00ee6e088574ee0d
impactpato <impactpato@gmail.com>
2022-07-05 19:15:15 -0700

    push it

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,17 +8,13 @@
 */
 
 int _isalpha(int c)
-
 {
+if ((c >= 'a' && c <= 'z') ||
+(c >= 'A' && c <= 'Z'))
+return (1);
 
-		if ((c >= 'a' && c <= 'z') ||
+else
 
-					    (c >= 'A' && c <= 'Z'))
-
-					return (1);
-
-			else
-
-						return (0);
+return (0);
 
 }
