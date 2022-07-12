4cd19c909937e5ebdbb38fa198b2cf3e9a9168c1
impactpato <impactpato@gmail.com>
2022-07-12 14:32:25 -0700

    home

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,27 @@
+/*
+ * File: 1-swap.c
+ * Auth: Impactpato
+ */
+
+
+
+nclude "main.h"
+
+
+
+/**
+ * _strlen - Entry point
+ * @s: parameter
+ * Return: lenth of string
+ */
+
+		int _strlen(char *s)
+
+{
+		int i = 0;
+		while (s[i] != '\0')
+		{
+			i++;
+		}
+			return (i);
+}
