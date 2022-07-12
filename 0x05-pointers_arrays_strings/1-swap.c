e7b18a58713e8f0161dfabeca2a4a26525673540
impactpato <impactpato@gmail.com>
2022-07-12 14:05:25 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+
+/**
+ *swap_int - Entry point
+ * @a: first parameter
+ * @b: second parameter
+ */
+
+void swap_int(int *a, int *b)
+
+{
+		int tem = *a;
+		*a = *b
+		*b = tem;
+
+}
