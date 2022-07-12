77edc6219cd20846c6c044a8e4e094ec3dea6ae8
impactpato <impactpato@gmail.com>
2022-07-12 13:12:42 -0700

    pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,13 +3,13 @@
 
 
 /**
- * reset_to_98 - Entry
+ reset_to_98 - Takes a pointer to an int and updates
  *
- *@n: variable as input
+ *             the value it points to to 98.
+ *
+ * @n: The pointer to an int.
  *
  */
-
-
 
 void reset_to_98(int *n)
 
