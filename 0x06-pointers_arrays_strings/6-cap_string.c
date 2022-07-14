a958c27dc5a1e360cc3538293dc42a7baaa42f38
impactpato <impactpato@gmail.com>
2022-07-14 16:01:37 -0700

    pointers and arrays

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,17 +1,32 @@
-#include "main.h" 
-/** string_toupper - cjhanges lowercase to uppercase
- *  @str: the string to change 
- *  Return: the string that change for uppercase 
+/** File: 6-cap_string.c 
+ * Auth: Impactpato 
  */ 
-char *string_toupper(char *str) { 
-	int i; 
-	i = 0; 
-	while (str[i] != '\0') {
-	       
-		if (str[i] >= 97 && str[i] <= 122) { 
-			str[i] = str[i] - 32; 
-		} 
-		i++; 
+#include "holberton.h" 
+/** cap_string - Capitalizes all words of a string. 
+ * @str: The string to be capitalized. 
+ *  Return: A pointer to the changed string. 
+ */ 
+char *cap_string(char *str) { 
+	int index = 0; 
+	while (str[index]) { 
+		while (!(str[index] >= 'a' && str[index] <= 'z')) 
+			index++; 
+		if (str[index - 1] == ' ' || 
+		   str[index - 1] == '\t' || 
+		   str[index - 1] == '\n' || 
+		   str[index - 1] == ',' || 
+		   str[index - 1] == ';' || 
+		   str[index - 1] == '.' || 
+		   str[index - 1] == '!' || 
+		   str[index - 1] == '?' || 
+		   str[index - 1] == '"' || 
+		   str[index - 1] == '(' || 
+		   str[index - 1] == ')' || 
+		   str[index - 1] == '{' || 
+		   str[index - 1] == '}' || 
+		   index == 0) 
+			str[index] -= 32; 
+		index++; 
 	} 
 	return (str); 
 }
