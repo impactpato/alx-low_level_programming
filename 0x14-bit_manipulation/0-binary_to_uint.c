d8b1ab8c15f7370f5331c550be6e1a673bdcc8f0
impactpato <106944634+impactpato@users.noreply.github.com>
2022-08-19 07:13:38 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,36 +1,68 @@
-/*
- * Auth: Impactpato
- * File: 0-binary_to_uint.c
- */
-
-#include "holberton.h"
+#include "main.h"
+#include <stdlib.h>
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 and 1 chars.
+ * _strlen - return the length of a string
+ * @str: a const pointer to the sting
  *
- * Return: If b is NULL or contains chars not 0 or 1 - 0.
- *         Otherwise - the converted number.
+ * Return: the string length
  */
+
+unsigned int _strlen(char const *str)
+{
+	unsigned int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+/**
+ * _pow - return the power of a number
+ * @a: the number to be raised
+ * @b: the exponent
+ *
+ * Return: the power of a number
+ */
+unsigned int _pow(unsigned int a, unsigned int b)
+{
+	unsigned int i, pow = 1;
+
+	if (b == 0)
+		return (1);
+
+	for (i = 0; i < b; i++)
+		pow *= a;
+
+	return (pow);
+}
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: a pointer to a string
+ *
+ * Return: the converted number
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+	unsigned int i, j, len, sum, basePwr;
 
-	if (b == '\0')
+	if (!b)
 		return (0);
 
-	for (len = 0; b[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-
-		num += (b[len] - '0') * mult;
-		mult *= 2;
 	}
 
-	return (num);
+	len = _strlen(b);
+	sum = 0;
+	for (i = 0, j = len; i < len; i++, j--)
+	{
+		basePwr = _pow(2, j - 1);
+		sum += (((b[i]) - '0') * basePwr);
+	}
+	return (sum);
 }
